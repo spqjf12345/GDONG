a921b2f58d22d13430856b983f2b979360f2d3f7
jouureee <jouureee@gmail.com>
2021-08-22 20:01:01 +0900

    info list edit

--- GDONG/AppDelegate.swift
+++ GDONG/AppDelegate.swift
@@ -12,8 +12,6 @@ import KakaoSDKUser
 import GoogleSignIn
 import AuthenticationServices
 import CoreLocation
-import AppTrackingTransparency
-import AdSupport
 import Firebase
 import Photos
 
@@ -27,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         KakaoSDKCommon.initSDK(appKey: "1cb2a37d6920168105b844b889d7766f") // native key
-        let signInCofig = GIDConfiguration.init(clientID: "966907908166-emcm81mpq4217qoqtkl9c3ndjcdl5to5.apps.googleusercontent.com")
+        _ = GIDConfiguration.init(clientID: "966907908166-emcm81mpq4217qoqtkl9c3ndjcdl5to5.apps.googleusercontent.com")
         
         self.locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
